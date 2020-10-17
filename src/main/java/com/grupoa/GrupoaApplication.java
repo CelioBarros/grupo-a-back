b77d8dd0539aaeac6b1768c254c9748feed9d85c
Célio Barros <celio.barros.filho@gmail.com>
2020-10-17 09:58:56 -0300

    Beggining project

---
+++ src/main/java/com/grupoa/GrupoaApplication.java
@@ -0,0 +1,13 @@
+package com.grupoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupoa/GrupoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
