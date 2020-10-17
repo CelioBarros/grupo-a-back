5700760c6c1e6d9e0dedc76a0f1f835748cdac9f
Célio Barros <celio.barros.filho@gmail.com>
2020-10-17 12:53:59 -0300

    Fix application config

--- src/main/java/com/grupoa/GrupoaApplication.java
+++ src/main/java/com/grupoa/GrupoaApplication.java
@@ -1,9 +1,14 @@
 package com.grupoa;
 
+import com.grupoa.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ApplicationProperties.class})
+@EnableJpaAuditing
 public class GrupoaApplication {
 
 	public static void main(String[] args) {
